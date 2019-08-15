28ed9d2917cb3063d09f33614814945e068c2855
yu.shevchenko <yu.shevchenko@temabit.com>
2019-08-15 09:37:44 +0300

    ку

--- LoyaltySignupAPISilpoAPP/SwaggerMethods.cs
+++ LoyaltySignupAPISilpoAPP/SwaggerMethods.cs
@@ -89,6 +89,7 @@ namespace LoyaltySignupAPISilpoAPP
             return SwaggerHelper.CommitPostRequest(SwaggerHelper.serviceUrl + relativeUrl, JsonConvert.SerializeObject(v));
         }
 
+
         // Проверка перехода по ссылке (для подтверждения e-mail)
         public static string VerifiedEmailAccessHash(string hashStr)
         {
